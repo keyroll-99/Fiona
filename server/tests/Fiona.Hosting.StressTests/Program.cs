666a80d1231dceee4c1e8819023b118cf29e3e89
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2024-03-19 20:55:25 +0100

    Configure nbomber

--- server/tests/Fiona.Hosting.StressTests/Program.cs
+++ server/tests/Fiona.Hosting.StressTests/Program.cs
@@ -12,15 +12,13 @@ fionaServer.Run();
 using var httpClient = new HttpClient()
 {
     BaseAddress = new Uri("http://localhost:7000/")
-}; 
+};
 
 var scenario = Scenario.Create("FionaServer", async context =>
 {
     var response = await httpClient.GetAsync("status-code/200");
     return response.IsSuccessStatusCode ? Response.Ok() : Response.Fail();
-}).WithLoadSimulations(
-    Simulation.KeepConstant(40_000, TimeSpan.FromSeconds(30))
-);
+});
 
 NBomberRunner
     .RegisterScenarios(scenario)
