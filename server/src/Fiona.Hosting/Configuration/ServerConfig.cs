90a64fb254b7fd948adc3806979bc6cfe2c093a7
keyroll-99 <kazmierczakkarol1999@gmail.com>
2024-03-29 19:50:54 +0100

    Fix running localhost on linux

--- server/src/Fiona.Hosting/Configuration/ServerConfig.cs
+++ server/src/Fiona.Hosting/Configuration/ServerConfig.cs
@@ -2,7 +2,7 @@ namespace Fiona.Hosting.Configuration;
 
 internal sealed class ServerConfig
 {
-    public string Domain { get; set; } = "http://127.0.0.1";
+    public string Domain { get; set; } = "http://localhost";
     public string Port { get; set; } = "7001";
     public string Environment { get; set; } = "Development";
     public bool ReturnFullErrorMessage { get; set; }
