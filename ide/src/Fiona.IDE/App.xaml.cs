a0a465ce4b723c508f8d0f007869cfab6794a2a6
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2024-04-03 21:13:44 +0200

    Add Fiona.GUI project

---
+++ ide/src/Fiona.IDE/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Fiona.IDE;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ ide/src/Fiona.IDE/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiona.IDE;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
