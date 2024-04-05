bfc8ac76e9affab983940e1b811960910be7bb2a
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2024-04-05 18:06:59 +0200

    Add cusomt behavior for drag and drop

---
+++ ide/src/Fiona.IDE/Components/Behavior/MyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Models.Base;
+
+namespace Fiona.IDE.Components.Behavior;
+
+public class MyBehavior : Blazor.Diagrams.Core.Behavior
+{
+    public MyBehavior(Diagram diagram) : base(diagram)
+    {
+        Diagram.PointerDown += OnPointerDown;
+    }
+
+    private void OnPointerDown(Model? model, Blazor.Diagrams.Core.Events.PointerEventArgs e)
+    {
+        if (model == null) // Canvas
+        {
+            Diagram.UnselectAll();
+        }
+        else if (model is SelectableModel sm)
+        {
+            Diagram.SelectModel(sm, true);
+        }
+    }
+
+    public override void Dispose()
+    {
+        Diagram.PointerDown -= OnPointerDown;
+    }
+}
